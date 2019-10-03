8ac4880fda9e7977866ca4a33567cd4bec798b90
tariktaci <tariktaci>
2019-10-02 12:13:37 -1200

    Lab 2 solutions

---
+++ Lab2/lab2Q2.js
@@ -0,0 +1,29 @@
+
+async function pluck(x){
+    try{
+ await function pl(x) {
+    if (x = true) {
+        let max = this[0]
+        for (let x in this) {
+            if (x > max) {
+                max = x;
+            }
+        }
+        console.log(max);
+    }
+    else{
+        let min= this[0];
+        for(let y in this){
+            if(y<min){
+                min=y;
+            }
+        }
+        console.log(y);
+    }
+}
+}
+catch(error){
+console.log(error);
+}
+}
+[1,2,3,4,5].pluck(true);
